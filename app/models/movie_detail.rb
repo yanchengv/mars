916eef6beab25b6b65b20df89791550d49a76f66
yancheng <zqalyc@163.com>
2016-10-14 11:35:34 +0800

    网盘下载

--- app/models/movie_detail.rb
+++ app/models/movie_detail.rb
@@ -3,6 +3,8 @@
       t.integer :episode  #剧集数
       t.string :url #影片下载地址
       t.string :name #剧集名称
+      t.string :wangpan_url #网盘地址
+      t.string :wangpan_password #网盘密码
 =end
 class MovieDetail < ApplicationRecord
   belongs_to :movie
--- db/migrate/20160928030156_create_movie_details.rb
+++ db/migrate/20160928030156_create_movie_details.rb
@@ -8,6 +8,8 @@ class CreateMovieDetails < ActiveRecord::Migration[5.0]
       t.integer :episode  #剧集数
       t.string :url
       t.string :url2
+      t.string :wangpan_url #网盘地址
+      t.string :wangpan_password #网盘密码
       t.string :name
       t.timestamps
     end
