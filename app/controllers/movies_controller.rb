94394c7769ab597898fe62a2a06a126efb1916c1
yancheng <zqalyc@163.com>
2016-10-17 15:01:02 +0800

    home nav

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < ApplicationController
 
+
+  def index
+    @selected_region = params[:region].present? ? params[:region] : '全部'
+    @selected_movie_tag = params[:movie_tag].present? ? params[:movie_tag] : '全部'
+    @selected_movie_type = params[:movie_type].present? ? params[:movie_type] : '全部'
+    @movies = Movie.paginate(:page => params[:page], :per_page => 10)
+  end
+
   def show
     @movie = Movie.includes(:movie_details)
                  .where(id: params[:id]).first
@@ -27,10 +35,20 @@ class MoviesController < ApplicationController
       values << params[:movie_type]
     end
 
-    @movies = Movie.includes(:movie_details).where(conditions.join(' and '),*values)
-                  .paginate(:page => params[:page], :per_page => 10)
+    if params[:grade].present?
+      @movies = Movie.includes(:movie_details).where(conditions.join(' and '),*values).order('grade desc')
+                    .paginate(:page => params[:page], :per_page => 10)
+    elsif params[:created_at].present?
+
+      @movies = Movie.includes(:movie_details).where(conditions.join(' and '),*values).order('created_at desc')
+                    .paginate(:page => params[:page], :per_page => 10)
+    else
+      @movies = Movie.includes(:movie_details).where(conditions.join(' and '),*values)
+                    .paginate(:page => params[:page], :per_page => 10)
+    end
     render 'homes/index'
   end
+
 
 
 end
