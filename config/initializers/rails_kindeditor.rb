cf590610f421474d2974c6a51a670d11883b1a94
yancheng <zqalyc@163.com>
2016-10-21 17:44:23 +0800

    uploads

--- config/initializers/rails_kindeditor.rb
+++ config/initializers/rails_kindeditor.rb
@@ -4,13 +4,13 @@ RailsKindeditor.setup do |config|
   # You can customize it , eg: config.upload_dir = 'this/is/my/folder'
   config.upload_dir = 'uploads'
 
-  # Allowed file types for upload.
+  # Allowed file types for uploads.
   config.upload_image_ext = %w[gif jpg jpeg png bmp]
   config.upload_flash_ext = %w[swf flv]
   config.upload_media_ext = %w[swf flv mp3 wav wma wmv mid avi mpg asf rm rmvb]
   config.upload_file_ext = %w[doc docx xls xlsx ppt htm html txt zip rar gz bz2]
 
-  # Porcess upload image size
+  # Porcess uploads image size
   # eg: 1600x1600 => 800x800
   #     1600x800  => 800x400
   #     400x400   => 400x400  # No Change
