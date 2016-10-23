ae4031719cb9393967c1039a350d977010a10fe4
yancheng <zqalyc@163.com>
2016-10-23 14:46:21 +0800

    movie desc

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
     @selected_region = params[:region].present? ? params[:region] : '全部'
     @selected_movie_tag = params[:movie_tag].present? ? params[:movie_tag] : '全部'
     @selected_movie_type = params[:movie_type].present? ? params[:movie_type] : '全部'
-    @movies = Movie.paginate(:page => params[:page], :per_page => 10)
+    @movies = Movie.order(created_at: :desc).paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
